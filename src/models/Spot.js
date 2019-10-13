bd5b2373ce93100c21e9e072e0ea72d00146f110
Matheus Pires <matheushenriquepires99@gmail.com>
2019-10-13 10:16:39 -0300

    first commit

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -1,14 +1,22 @@
-const mogoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const SpotSchema = new mogoose.Schema({ 
-    thumbnail: String,
-    company: String,
-    price: Number,
-    techs: [String],
-    user: {
-        type: mogoose.Schema.Types.ObjectId,
-        ref: "User"
-    }
+const SpotSchema = new mongoose.Schema({
+  thumbnail: String,
+  company: String,
+  price: Number,
+  techs: [String],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+}, {
+  toJSON: {
+    virtuals: true,
+  },
 });
 
-module.exports = mogoose.model("Spot", SpotSchema);
+SpotSchema.virtual('thumbnail_url').get(function() {
+  return `http://localhost:3333/files/${this.thumbnail}`
+})
+
+module.exports = mongoose.model('Spot', SpotSchema);
--- src/server.js
+++ src/server.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
+const socketio = require("socket.io");
+const http = require("http");
 
 const routes = require("./routes"); 
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect("mongodb+srv://matheus:matheus@omnistack-zlnx0.mongodb.net/semana09?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+});
+
+const connectedUsers = {};
+
+io.on("connection", socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
 });
 
 // GET, POST, PUT, DELETE
@@ -20,6 +40,7 @@ mongoose.connect("mongodb+srv://matheus:matheus@omnistack-zlnx0.mongodb.net/sema
 
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333); 
